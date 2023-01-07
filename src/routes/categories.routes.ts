718bbac33c906583109dc22e747a647bae338eaa
NGustavo011 <gsn181200@gmail.com>
2023-01-06 21:53:01 -0300

    adding services layer and repository contracts

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,23 @@
-import { Router } from 'express';
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { Router } from "express";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/category/CreateCategoryService";
 
 const categoriesRoutes = Router();
 
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.get('/', (request, response) => {
-  const categories = categoriesRepository.list();
-  return response.json(categories);
+categoriesRoutes.get("/", (request, response) => {
+    const categories = categoriesRepository.list();
+    return response.json(categories);
 });
 
-categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exists' });
-  }
-
-  categoriesRepository.create({ name, description });
-  return response.status(201).send();
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;
+    const createCategoryService = new CreateCategoryService(
+        categoriesRepository
+    );
+    createCategoryService.execute({ name, description });
+    return response.status(201).send();
 });
 
 export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
+import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello world' });
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello world" });
 });
 
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+    return response.json({ name });
 });
 
-app.listen(3333, () => console.log('Server is running!'));
+app.listen(3333, () => console.log("Server is running!"));
